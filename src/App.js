a3bb0cb93c95e9feb8fb3c2e5c46d7e81b6ae6a4
QaiserGul <qaisergul000@gmail.com>
2022-02-27 18:44:23 +0530

    landing-start

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Signup from "./pages/Signup";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
 import JNF from "./pages/JNF";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/landing" element={<Landing />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/dashboard" element={<Dashboard />} />
